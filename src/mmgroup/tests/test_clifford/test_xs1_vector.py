ab48fcf06638d51006f1267a1674a2f8faf137f2
Martin-Seysen <m.seysen@gmx.de>
2020-10-15 17:10:45 +0200

    Bugfixes Linux version, tests for xsp2co1.c enhanced

--- src/mmgroup/tests/test_clifford/test_xs1_vector.py
+++ src/mmgroup/tests/test_clifford/test_xs1_vector.py
@@ -15,6 +15,7 @@ from mmgroup.structures.qs_matrix import QStateMatrix
 from mmgroup.structures.xs1_co1 import Xs12_Co1, str_leech3
 from mmgroup.structures.xs1_co1 import get_error_pool
 from mmgroup.structures.autpl import AutPL
+from mmgroup.mat24 import MAT24_ORDER
 from mmgroup.tests.spaces.clifford_space import Space_ZY
 from mmgroup.clifford12 import xp2co1_chain_short_3, xp2co1_elem_to_qs
 from mmgroup.clifford12 import xp2co1_short_2to3, xp2co1_short_3to2
@@ -32,19 +33,44 @@ STD_V3  = 0x8000004
 
 
 #####################################################################
-# Convert matrix of type QStateMatrix to a complex matrix
+# Creating test vectors
 #####################################################################
+
+
+def rand_tuple(tag):
+    if tag in "BC":
+        i0 = i1 = randint(0, 23)
+        while i1 == i0:
+            i1 = randint(0, 23)
+        return tag, i0, i1
+    if tag == "T":
+        return tag, randint(0, 758), randint(0, 63),
+    if tag in "XY":
+        return tag, randint(0, 2047), randint(0, 23),
+    if tag == "d":
+        return tag, randint(0, 0xfff)
+    if tag == "p":
+        return tag, randint(0,  MAT24_ORDER - 1)
+    if tag in  "xy":
+        return tag, randint(0, 0x1fff)
+    if tag ==  "l":
+        return tag, randint(1, 2)
+    raise ValueError("Illegal tag %s for group or vector" % tag)
+        
+
+def rand_element(s):
+    return [rand_tuple(tag) for tag in s]
 
 def create_test_vectors():
     vector_data = [ 
-      (0x001, ("B", 2, 3)),
-      (0x001, ("C", 0, 16)),
-      (0xabf, ("C", 15, 19)),
-      (0x001, ("T", 643, 51)),
-      (0x001, ("T", 199, 7)),
-      (0x001, ("X", 0, 23)),
-      (0x001, ("X", 137, 23)),
-      (0x001, ("X", 1897, 1)),
+      (0x001, (1, "B", 2, 3)),
+      (0x001, (1, "C", 0, 16)),
+      (0xabf, (1, "C", 15, 19)),
+      (0x001, (1, "T", 643, 51)),
+      (0x001, (1, "T", 199, 7)),
+      (0x001, (1, "X", 0, 23)),
+      (0x001, (1, "X", 137, 23)),
+      (0x001, (1, "X", 1897, 1)),
     ]
     group_data = [
       [('d', 0x124)],
@@ -56,17 +82,19 @@ def create_test_vectors():
     for i in range(1):
         p = {0:2, 1:3, 2:0, 3:1, 4:4, 6:6, 8:8}
         #print("AutPL", [hex(x) for x in AutPL(p).rep])
-        yield 256, ("B", 2, 3),  [("p", p)]
+        yield 256, (1, "B", 2, 3),  [("p", p)]
     for x4096, x24 in vector_data:
         for g in group_data:
             yield x4096, x24, g
 
+    for x in "BCTX":
+        for j in range(50):
+            sign = -1**j
+            d = randint(0, 0xfff)
+            t = rand_tuple(x)
+            yield d, ((sign,) + t),  rand_element("dpdp")    
 
-def tuple_to_leech3(tag, i0, i1):
-    sign = 1
-    if tag[0] == '-':
-        sign = -1
-        tag = tag[1:]
+def tuple_to_leech3(sign, tag, i0, i1):
     x2 = MMSpace3.index_to_short_mod2(tag, i0, i1)
     x3 = xp2co1_short_2to3(x2)
     if sign == -1:
@@ -122,7 +150,7 @@ def test_vector(verbose = 0):
         if verbose:
             print("\nTEST %s" % (ntest+1))
             print("vector =", x4096, x24)
-        vm = Space_ZY.unit(x4096, x24)
+        vm = x24[0] * Space_ZY.unit(x4096, x24[1:])
         vm_old = vm.copy()
         if verbose:
             print("vm =", vm.as_tuples())
