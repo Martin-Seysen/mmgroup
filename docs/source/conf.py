8e3336d9816065fcd009bfa9bd48d0564f012575
Martin-Seysen <m.seysen@gmx.de>
2020-11-26 18:45:13 +0100

    Changed to spinx theme 'sphinx_rtd_theme'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 import subprocess
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src')))
 
@@ -64,6 +66,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+   'sphinx_rtd_theme',
    'sphinx.ext.autodoc',
    'sphinxcontrib.bibtex',
     'breathe',
@@ -83,7 +86,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
