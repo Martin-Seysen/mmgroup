acd7e6a27b899facdb70e4da732102897ef6dd90
Martin-Seysen <m.seysen@gmx.de>
2022-10-13 12:09:49 +0200

    Temporary change for testing macOS on GitHub

--- src/mmgroup/tests/test_mmgroup/test_mul_MM.py
+++ src/mmgroup/tests/test_mmgroup/test_mul_MM.py
@@ -27,14 +27,27 @@ def mul_testcases(ncases = 1):
          for i in range(ncases):
               yield MM('r', complexity)
 
+@pytest.mark.mmm 
 @pytest.mark.mmgroup 
-def test_mul_MM():
+def test_mul_MM(verbose = 1):
     print("Test multiplcation in monster group in class MM")
+    TEXT = "Test %d: multiply random vector v (mod 3) with g1 and g2"
     V = MMV(3)
+    n = 1
     for g1 in mul_testcases():
         v = V("R")
         for g2 in  mul_testcases():
+            if verbose:
+                print(TEXT % n)
+                print(" g1 =", g1)
+                print(" g2 =", g2)
+                print(" Computing (v * g1) * g2")
             v1 = (v * g1) * g2
             v2 = v * (g1 * g2)
+            if verbose:
+                print(" Computing  v * (g1 * g2)")
             assert v1 == v2
+            if verbose:
+                print(" passed")
+                n += 1
 
