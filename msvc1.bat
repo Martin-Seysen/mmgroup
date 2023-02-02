642485299026527589fa78dee510952328d295ef
Martin-Seysen <m.seysen@gmx.de>
2023-02-02 13:34:19 +0100

    More adjustments made for python 3.11

--- msvc1.bat
+++
@@ -1,9 +0,0 @@
-rem Activate the MSVC compiler for a Windows 64-bit command line 
-rem The path may be different on another PC or after reinstallation!!
-rem The current MSVC compiler version has been downloaded at 2020-06-14 from
-rem "https://visualstudio.microsoft.com/de/thank-you-downloading-visual-studio/?sku=BuildTools&rel=16"
-rem instructions see:
-rem "https://www.scivision.dev/python-windows-visual-c-14-required/"
-rem But nobody knows where to get it next year or for the next python version!!
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-
