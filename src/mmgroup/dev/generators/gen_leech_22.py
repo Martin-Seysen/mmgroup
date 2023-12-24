4aaf9b62c7358d93ad410e4badc2fc77eb517169
Dr. Martin Seysen <m.seysen@gmx.de>
2023-12-24 16:11:19 +0100

    Documentation updated

--- src/mmgroup/dev/generators/gen_leech_22.py
+++ src/mmgroup/dev/generators/gen_leech_22.py
@@ -1,8 +1,17 @@
+"""Compute tables für functions in file gen_reduce_22.c"""
+
 from mmgroup.dev.mat24.mat24_ref import Mat24 as m
 
 
 
 def iter_octads_22():
+    """Iterate over the 77 octads containing entries 2 and 3
+
+    The function yields the 77 octads o containing the entries
+    2 and 3 as bit vectors. It also yields a list bl of five
+    entries of octad o, excludin the entries 2 and 3.
+    So the function yields 77 peits (o, bl)
+    """
     n = 0
     for i in range(759):
         o = m.octad_to_vect(i) 
@@ -16,6 +25,19 @@ def iter_octads_22():
 
 
 def iter_vectors_22():
+    """Yield Leech lattice mod 2 vectors from function iter_octads_22
+
+    For each of the 77 octads obtained from function iter_octads_22()
+    there are 16 type-2 vectors v2 in the Leech lattice mod 2 such
+    v2 + w2  is also of type 2. Here w2 is the vector
+    (0, 0, 4, -4, 0,...,0) in the Leech lattice mod 2.
+    The function returns quintuples (x1, x2, x3, x4, ov)
+    such that ov + u1*x1 + u2*x2 + u3*x3 + u4*x4 + u5*Omega is
+    a type_2 vector as described above for any u1, u2, u3, u4
+    in [0, 1]. Here u5 depends on  u1, u2, u3, u4 as follows:
+    u5 is 1 if u1 + u2 + u3 + u4 in [0, 1, 4]; and u5 = 0
+    otherwise. Here x1,...,x4 are even cocode vectors.
+    """
     for o, bl in iter_octads_22():
         og = m.vect_to_gcode(o)
         ov = (og << 12) + (m.theta_table[og & 0x7ff] & 0xfff)
@@ -28,10 +50,29 @@ def iter_vectors_22():
 
 
 def compress(x):
+    """Compress element obtained from iter_vectors_22()
+
+    A 24-bit integer obtained from function iter_vectors_22()
+    decribes an element x of the Leech lattice mod 2. By
+    construction, bits 9 and 11 of such a value x are always zero.
+    We compress x by mapping bits (23,...,12,10,8,...,0) of x
+    to bits (21,...,10,9,8,...,0). We return that compresses value.
+    """
     return (x & 0x1ff) + ((x & 0x400) >> 1) + ((x & 0xfff000) >> 2)
 
 
 def make_table():
+    """Make table from function iter_vectors_22()
+
+    The function creates a table of 77 64-bit integers. Any such
+    integers correspond to a (compressed) quintuple
+    (x1, x2, x3, x4, ov) as obtained by function iter_vectors_22().
+    Here function compress() is used to compress x1, x2, x3, x4
+    from 12 to 10 bit and to compress ov from 24 to 22 bits.
+
+    The value x_i is stored in bits 10*i+9,...,10*i from an entry;
+    and ov is stored in bits 61,...,40 of an entry.
+    """
     tbl = []
     for lst in iter_vectors_22():
         a = 0
@@ -45,6 +86,15 @@ def make_table():
 
 
 def w_mod4():
+    """Compute a certain parity function
+
+    The function returns a value a such that bit i of a is
+    set if and only if the integer i has bit parity 0, 1, or 4;
+    for 0 <= i < 16.
+
+    So this function may be used to compute the bit u5 defined
+    in function iter_vectors_22().
+    """
     a = 0
     for i in range(16):
         if m.bw24(i) in [0,1,4]:
